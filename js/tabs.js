3171b9c3802aaab1d99fb70b4249959290ee9811
DayTroy <70051506+DayTroy@users.noreply.github.com>
2022-03-18 20:24:00 +0300

    tabs.js changed title's innerText
    Change the title of the browser tab to the text from the title above the tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,10 @@
 const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescriptions = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
+const title = document.querySelector('title');
 
 const changeContent = (array, value) => {
+
     array.forEach((elem) => {
         if(elem.dataset.tabsField === value){
             elem.classList.remove('hidden');
@@ -24,8 +26,11 @@ tabButtons.forEach((tabButton, index) => {
         tabButtons.forEach((btn) => {
             if (btn == event.target) {
                 btn.classList.add('design-list__item_active');
+                title.innerText = tabButtons[index].innerText
+                
             } else {
                 btn.classList.remove('design-list__item_active');
+                title.innerText = tabButtons[index].innerText
             }
         })
 
